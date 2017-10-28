49f79285280ca7b3b3067ad24f1736c64b54a3ae
sarthakmathur <sarthakmathur97@gmail.com>
2017-10-28 13:54:19 +0530

    changes

---
+++ app/src/main/java/com/example/android/vihaanhack/Activities/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.vihaanhack.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.vihaanhack.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
